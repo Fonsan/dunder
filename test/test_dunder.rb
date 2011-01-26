cb8cd8affa9e0c33251200a3e1b3c79aaafd5f41
Fonsan <fonsan@gmail.com>
2011-01-26 16:19:45 +0100

    Tests for non-blocking

--- test/test_dunder.rb
+++ test/test_dunder.rb
@@ -1,14 +1,17 @@
 require 'helper'
-
+require 'timeout'
 class TestDunder < Test::Unit::TestCase
   should "have some simple testing" do
-    
      b = "bar"
-     
-     lazy_b = Dunder.load(String) {
-        "bar"
-      }
-     
+     lazy_b = nil
+     assert_nothing_raised do
+       Timeout::timeout(0.5) do
+       lazy_b = Dunder.load(String) {
+          sleep 1
+          "bar"
+        }
+       end
+     end
      assert_equal b,lazy_b
      puts lazy_b.class
      assert_equal b.class, lazy_b.class
